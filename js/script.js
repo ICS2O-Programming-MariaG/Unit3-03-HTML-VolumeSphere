284cc72bee2ca75770d630a5480eacf5d75f895a
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-13 14:38:44 +0000

    Added calculations in JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Maria G
 // Created on: April 13, 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function radiusSent() {
+  let radiusSphere = parseFloat(document.getElementById("radius").value);
+  const pi = Math.PI;
+  let volume = radiusSphere ** 3 * pi * 4 / 3;
+  document.getElementById("results").innerHTML = "The volume of the sphere is " + volume.toFixed(2) + "cm<sup>3</sup>.";
+}
